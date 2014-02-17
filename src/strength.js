fb231d46e1015acd902bf0e0ceaa8a8871a2861b
Craig Morey <craig.morey@uswitch.com>
2014-02-17 10:24:42 +0000

    general tidy up of extra lines/tabs

--- src/strength.js
+++ src/strength.js
@@ -13,12 +13,10 @@
             strengthButtonClass: 'button_strength',
             strengthButtonText: 'Show Password',
             strengthButtonTextToggle: 'Hide Password',
-            weak: /^[a-zA-Z0-9]{6,}$/, // minimum acceptable: min 6 chars, any char/number, no spaces
+            weak: /^[a-zA-Z0-9]{6,}$/, // minimum acceptable: min 6 chars, any caps/no-caps/number, no spaces
             medium: /^(?=.*\d)(?=.*[a-z])(?!.*\s).{8,}$|^(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,}$/, // reasonable: min 8 chars, either caps+no-caps or no-caps+numbers, no spaces
             strong: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,}$/ // strong: min 8 chars, caps+no-caps+numbers, no spaces
         };
-
-       // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
 
     function Plugin( element, options ) {
         this.element = element;
@@ -38,33 +36,29 @@
             var strongTest = this.options.strong;
 
             function check_strength(thisval,thisid){
-              var thismeter = $('div[data-meter="'+thisid+'"]');
-              if (thisval.length==0) {
-                thismeter.removeClass().text('strength');
-              } else if (thisval.search(strongTest)>=0) {
-                thismeter.removeClass().addClass('strong').text('strong');
-              } else if (thisval.search(meduimTest)>=0) {
-                thismeter.removeClass().addClass('medium').text('medium');
-              } else if (thisval.search(weakTest)>=0) {
-                thismeter.removeClass().addClass('weak').text('weak');
-              } else {
-                thismeter.removeClass().addClass('veryweak').text('very week');
-              }
+                var thismeter = $('div[data-meter="'+thisid+'"]');
+                if (thisval.length==0) {
+                  thismeter.removeClass().text('strength');
+                } else if (thisval.search(strongTest)>=0) {
+                  thismeter.removeClass().addClass('strong').text('strong');
+                } else if (thisval.search(meduimTest)>=0) {
+                  thismeter.removeClass().addClass('medium').text('medium');
+                } else if (thisval.search(weakTest)>=0) {
+                  thismeter.removeClass().addClass('weak').text('weak');
+                } else {
+                  thismeter.removeClass().addClass('veryweak').text('very week');
+                }
             }
 
             var isShown = false;
             var strengthButtonText = this.options.strengthButtonText;
             var strengthButtonTextToggle = this.options.strengthButtonTextToggle;
-
             thisid = this.$elem.attr('id');
-
             this.$elem.addClass(this.options.strengthClass).attr('data-password',thisid).after('<input style="display:none" class="'+this.options.strengthClass+'" data-password="'+thisid+'" type="text" name="" value=""><a data-password-button="'+thisid+'" href="" class="'+this.options.strengthButtonClass+'">'+this.options.strengthButtonText+'</a><div class="'+this.options.strengthMeterClass+'"><div data-meter="'+thisid+'">Strength</div></div>');
-             
             this.$elem.bind('keyup keydown', function(event) {
                 thisval = $('#'+thisid).val();
                 $('input[type="text"][data-password="'+thisid+'"]').val(thisval);
                 check_strength(thisval,thisid);
-                
             });
 
              $('input[type="text"][data-password="'+thisid+'"]').bind('keyup keydown', function(event) {
@@ -72,44 +66,23 @@
                 console.log(thisval);
                 $('input[type="password"][data-password="'+thisid+'"]').val(thisval);
                 check_strength(thisval,thisid);
-                
             });
-
-
 
             $(document.body).on('click', '.'+this.options.strengthButtonClass, function(e) {
                 e.preventDefault();
-
-               thisclass = 'hide_'+$(this).attr('class');
-
-
-
-
+                thisclass = 'hide_'+$(this).attr('class');
                 if (isShown) {
                     $('input[type="text"][data-password="'+thisid+'"]').hide();
                     $('input[type="password"][data-password="'+thisid+'"]').show().focus();
                     $('a[data-password-button="'+thisid+'"]').removeClass(thisclass).html(strengthButtonText);
                     isShown = false;
-
                 } else {
                     $('input[type="text"][data-password="'+thisid+'"]').show().focus();
                     $('input[type="password"][data-password="'+thisid+'"]').hide();
                     $('a[data-password-button="'+thisid+'"]').addClass(thisclass).html(strengthButtonTextToggle);
                     isShown = true;
-   
                 }
-
-
-               
             });
-
-
-         
-            
-        },
-
-        yourOtherFunction: function(el, options) {
-            // some logic
         }
     };
 
