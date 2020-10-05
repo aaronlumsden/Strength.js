401d5bafdd435879d6be6e643f8537e2c90a1607
d.yermakov@codetiburon.com <Dmitry Yermakov>
2020-10-05 10:57:14 +0300

    - Modified file formatting. - Added strength level translations.

--- src/strength.js
+++ src/strength.js
@@ -4,7 +4,7 @@
  * Further changes, comments: @aaronlumsden
  * Licensed under the MIT license
  */
-;(function ( $, window, document, undefined ) {
+;(function ($, window, document, undefined) {
 
     var pluginName = "strength",
         defaults = {
@@ -12,15 +12,19 @@
             strengthMeterClass: 'strength_meter',
             strengthButtonClass: 'button_strength',
             strengthButtonText: 'Show Password',
-            strengthButtonTextToggle: 'Hide Password'
+            strengthButtonTextToggle: 'Hide Password',
+            veryWeakLevelText: 'very weak',
+            weakLevelText: 'weak',
+            mediumLevelText: 'medium',
+            strongLevelText: 'strong'
         };
 
-       // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
+    // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
 
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
         this.element = element;
         this.$elem = $(this.element);
-        this.options = $.extend( {}, defaults, options );
+        this.options = $.extend({}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
@@ -28,135 +32,135 @@
 
     Plugin.prototype = {
 
-        init: function() {
-
-
+        init: function () {
             var characters = 0;
             var capitalletters = 0;
             var loweletters = 0;
             var number = 0;
             var special = 0;
 
-            var upperCase= new RegExp('[A-Z]');
-            var lowerCase= new RegExp('[a-z]');
+            var upperCase = new RegExp('[A-Z]');
+            var lowerCase = new RegExp('[a-z]');
             var numbers = new RegExp('[0-9]');
             var specialchars = new RegExp('([!,%,&,@,#,$,^,*,?,_,~])');
 
             function GetPercentage(a, b) {
-                    return ((b / a) * 100);
-                }
-
-                function check_strength(thisval,thisid){
-                    if (thisval.length > 8) { characters = 1; } else { characters = -1; };
-                    if (thisval.match(upperCase)) { capitalletters = 1} else { capitalletters = 0; };
-                    if (thisval.match(lowerCase)) { loweletters = 1}  else { loweletters = 0; };
-                    if (thisval.match(numbers)) { number = 1}  else { number = 0; };
-
-                    var total = characters + capitalletters + loweletters + number + special;
-                    var totalpercent = GetPercentage(7, total).toFixed(0);
-
-                    if (!thisval.length) {total = -1;}
-
-                    get_total(total,thisid);
-                }
-
-            function get_total(total,thisid){
-
-                var thismeter = $('div[data-meter="'+thisid+'"]');
-                    if (total <= 1) {
-                   thismeter.removeClass();
-                   thismeter.addClass('veryweak').html('very weak');
-                } else if (total == 2){
-                    thismeter.removeClass();
-                   thismeter.addClass('weak').html('weak');
-                } else if(total == 3){
-                    thismeter.removeClass();
-                   thismeter.addClass('medium').html('medium');
-
-                } else {
-                     thismeter.removeClass();
-                   thismeter.addClass('strong').html('strong');
-                }
-                
-                if (total == -1) { thismeter.removeClass().html('Strength'); }
+                return ((b / a) * 100);
             }
 
+            function check_strength(thisval, thisid) {
+                if (thisval.length > 8) {
+                    characters = 1;
+                } else {
+                    characters = -1;
+                }
 
+                if (thisval.match(upperCase)) {
+                    capitalletters = 1
+                } else {
+                    capitalletters = 0;
+                }
 
+                if (thisval.match(lowerCase)) {
+                    loweletters = 1
+                } else {
+                    loweletters = 0;
+                }
 
+                if (thisval.match(numbers)) {
+                    number = 1
+                } else {
+                    number = 0;
+                }
+
+                var total = characters + capitalletters + loweletters + number + special;
+                var totalpercent = GetPercentage(7, total).toFixed(0);
+
+                if (!thisval.length) {
+                    total = -1;
+                }
+
+                get_total(total, thisid);
+            }
+
+            function get_total(total, thisid) {
+
+                var thismeter = $('div[data-meter="' + thisid + '"]');
+                if (total <= 1) {
+                    thismeter.removeClass();
+                    thismeter.addClass('veryweak').html(this.options.veryWeakLevelText);
+                } else if (total == 2) {
+                    thismeter.removeClass();
+                    thismeter.addClass('weak').html(this.options.weakLevelText);
+                } else if (total == 3) {
+                    thismeter.removeClass();
+                    thismeter.addClass('medium').html(this.options.mediumLevelText);
+                } else {
+                    thismeter.removeClass();
+                    thismeter.addClass('strong').html(this.options.strongLevelText);
+                }
+
+                if (total == -1) {
+                    thismeter.removeClass().html('Strength');
+                }
+            }
 
             var isShown = false;
             var strengthButtonText = this.options.strengthButtonText;
             var strengthButtonTextToggle = this.options.strengthButtonTextToggle;
 
-
             thisid = this.$elem.attr('id');
 
-            this.$elem.addClass(this.options.strengthClass).attr('data-password',thisid).after('<input style="display:none" class="'+this.options.strengthClass+'" data-password="'+thisid+'" type="text" name="" value=""><a data-password-button="'+thisid+'" href="" class="'+this.options.strengthButtonClass+'">'+this.options.strengthButtonText+'</a><div class="'+this.options.strengthMeterClass+'"><div data-meter="'+thisid+'">Strength</div></div>');
-             
-            this.$elem.bind('keyup keydown', function(event) {
-                thisval = $('#'+thisid).val();
-                $('input[type="text"][data-password="'+thisid+'"]').val(thisval);
-                check_strength(thisval,thisid);
-                
+            this.$elem.addClass(this.options.strengthClass).attr('data-password', thisid).after('<input style="display:none" class="' + this.options.strengthClass + '" data-password="' + thisid + '" type="text" name="" value=""><a data-password-button="' + thisid + '" href="" class="' + this.options.strengthButtonClass + '">' + this.options.strengthButtonText + '</a><div class="' + this.options.strengthMeterClass + '"><div data-meter="' + thisid + '">Strength</div></div>');
+
+            this.$elem.bind('keyup keydown', function (event) {
+                thisval = $('#' + thisid).val();
+                $('input[type="text"][data-password="' + thisid + '"]').val(thisval);
+                check_strength(thisval, thisid);
             });
 
-             $('input[type="text"][data-password="'+thisid+'"]').bind('keyup keydown', function(event) {
-                thisval = $('input[type="text"][data-password="'+thisid+'"]').val();
+            $('input[type="text"][data-password="' + thisid + '"]').bind('keyup keydown', function (event) {
+                thisval = $('input[type="text"][data-password="' + thisid + '"]').val();
                 console.log(thisval);
-                $('input[type="password"][data-password="'+thisid+'"]').val(thisval);
-                check_strength(thisval,thisid);
-                
+                $('input[type="password"][data-password="' + thisid + '"]').val(thisval);
+                check_strength(thisval, thisid);
             });
 
-
-
-            $(document.body).on('click', '.'+this.options.strengthButtonClass, function(e) {
+            $(document.body).on('click', '.' + this.options.strengthButtonClass, function (e) {
                 e.preventDefault();
 
-               thisclass = 'hide_'+$(this).attr('class');
-
-
-
+                thisclass = 'hide_' + $(this).attr('class');
 
                 if (isShown) {
-                    $('input[type="text"][data-password="'+thisid+'"]').hide();
-                    $('input[type="password"][data-password="'+thisid+'"]').show().focus();
-                    $('a[data-password-button="'+thisid+'"]').removeClass(thisclass).html(strengthButtonText);
+                    $('input[type="text"][data-password="' + thisid + '"]').hide();
+                    $('input[type="password"][data-password="' + thisid + '"]').show().focus();
+                    $('a[data-password-button="' + thisid + '"]').removeClass(thisclass).html(strengthButtonText);
                     isShown = false;
 
                 } else {
-                    $('input[type="text"][data-password="'+thisid+'"]').show().focus();
-                    $('input[type="password"][data-password="'+thisid+'"]').hide();
-                    $('a[data-password-button="'+thisid+'"]').addClass(thisclass).html(strengthButtonTextToggle);
+                    $('input[type="text"][data-password="' + thisid + '"]').show().focus();
+                    $('input[type="password"][data-password="' + thisid + '"]').hide();
+                    $('a[data-password-button="' + thisid + '"]').addClass(thisclass).html(strengthButtonTextToggle);
                     isShown = true;
-   
                 }
-
-
-               
             });
-
-
-         
-            
         },
 
-        yourOtherFunction: function(el, options) {
+        yourOtherFunction: function (el, options) {
             // some logic
         }
     };
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
+    $.fn[pluginName] = function (options) {
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
             }
         });
     };
 
-})( jQuery, window, document );
+})(jQuery, window, document);
 
 
